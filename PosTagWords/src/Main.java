0e577b05a18d7cad4f8e2f91bbe0c8892e390da8
Andreas Pålsson <apals@kth.se>
2015-05-19 09:23:08 +0200

    Ignore dumps

--- PosTagWords/src/Main.java
+++ PosTagWords/src/Main.java
@@ -1,16 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 
 public class Main {
 
+	private File file;
+	private Scanner scanner;
+	
 	public static void main(String[] args) {
 		MaxentTagger t = new MaxentTagger("tagger/english-left3words-distsim.tagger");
 		String sentence = "Jemil is a warrior. Mike has a big leg. The wall is cool.";
+		
+		openFile(args[0]);
 		
 		System.out.println();
 		System.out.println(sentence);
 		System.out.println(t.tagString(sentence));
 	}	
+	
+	private static void openFile(String filename) {
+		
+	}
 	
 }
 
