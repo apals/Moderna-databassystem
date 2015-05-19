6c199236644987fbc3628721cbd0403f0153ffcc
jb <jb@jb-Aspire-5750G.(none)>
2015-05-19 08:51:41 +0200

    This program creates sentence table

---
+++ tokenfixer/main.cpp
@@ -0,0 +1,29 @@
+
+#include <fstream>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h> 
+//g++ main.cpp
+// a.out <article id> <filename> 
+int main(int argc, char* argv[]) { 
+    if(argc < 2 ) {
+        std::cout<<"insert <article id> <filename>"<<std::endl;
+    }
+    //std::cout<<argv[2]<<std::endl;    
+    std::string line;
+    std::ifstream myfile (argv[2]);
+    long sentence_id = 1;
+    int article_id = atoi(argv[1]);
+
+    if (myfile.is_open()){
+        while ( getline (myfile,line) )
+        {
+          std::cout <<article_id <<"."<<sentence_id <<"."<<line << '\n';
+          sentence_id++;
+        }
+        myfile.close();
+    }else 
+        std::cout << "Unable to open file"<<std::endl; 
+
+    return 0;
+}
